21c29d443a995d956e6a6de7e60c52089a63ba2e
diaznicolasandres1 <diaz.nicolasandres@gmail.com>
2020-09-13 12:43:39 -0300

    Clean architecture image + swagger dlls

---
+++ Mutantes.API/Models/MessageReponseHandler.cs
@@ -0,0 +1,63 @@
+﻿using Mutantes.API.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mutantes.API.Models
+{
+    public static class MessageReponseHandler
+    {
+
+        public static MessageReponse CustomBadRequest(string message)
+        {
+            return new MessageReponse
+            {
+                status = 400,
+                tittle = "Bad Request.",
+                message = message
+            };
+        }
+
+        public static MessageReponse MutantFound()
+        {
+            return new MessageReponse
+            {
+                status = 200,
+                tittle = "Dna analisis result",
+                message = "DNA corresponds to a mutant"
+            };
+        }
+
+        public static MessageReponse HumanFound()
+        {
+            return new MessageReponse
+            {
+                status = 403,
+                tittle = "Dna analisis result",
+                message = "DNA corresponds to a human"
+            };
+        }
+
+        public static MessageReponse InternalServerError(string message)
+        {
+            return new MessageReponse
+            {
+                status = 500,
+                tittle = "Internal server errro",
+                message = message
+            };
+        }
+
+
+        public static MessageReponse UnsupportedMediaType(string message)
+        {
+            return new MessageReponse
+            {
+                status = 415,
+                tittle = "UnsupportedMediaType",
+                message = message
+            };
+        }
+    }
+}
